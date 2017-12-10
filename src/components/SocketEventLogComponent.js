a72f3f73de2c366ba2178ae7cf82527aa9ebb428
Petr Hejna <Achse0@gmail.com>
2017-12-10 13:38:59 +0100

    firt untested attemps for socket

--- src/components/SocketEventLogComponent.js
+++ src/components/SocketEventLogComponent.js
@@ -1,13 +1,41 @@
 import React from "react"
+import socket from '../socket'
 
 class SocketEventLogComponent extends React.Component {
+  constructor() {
+    super()
+    this.state = {log: []}
+  }
+
+  logMessage(message) {
+    const log = {...this.state.log}
+    log.push({message: message, timestamp: new Date().getTime()})
+    this.setState({log})
+  }
+
+  componentWillMount() {
+    socket.on('connect', () => {
+      this.logMessage('Connect')
+    })
+    socket.on('event', (data) => {
+      this.logMessage(data)
+    })
+    socket.on('disconnect', () => {
+      this.logMessage('Connect')
+    })
+  }
+
+  componentWillUnmount() {
+  }
 
   render() {
 
     return <div>
-      <code>
-        yolo
-      </code>
+      <ul>
+        {this.state.log.map(function (result, index) {
+          return <li><code>[{index}] {result.timestamp}: {result.message}</code></li>
+        })}
+      </ul>
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'normalize.css/normalize.css';
-import '@blueprintjs/core/dist/blueprint.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import 'normalize.css/normalize.css'
+import '@blueprintjs/core/dist/blueprint.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+require('dotenv').config()
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+registerServiceWorker()
