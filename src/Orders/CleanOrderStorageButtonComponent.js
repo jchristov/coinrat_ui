696738c32dc3b83ac2fb6c58ba537aceb77975dc
Petr Hejna <Achse0@gmail.com>
2018-01-04 09:56:57 +0100

    add tooltip for run strategy and clear-order-storage button

--- src/Orders/CleanOrderStorageButtonComponent.js
+++ src/Orders/CleanOrderStorageButtonComponent.js
@@ -1,18 +1,20 @@
 // @flow
 import React from "react"
-import {Button} from "@blueprintjs/core/dist/components/button/buttons"
+import {Button, Position, Tooltip} from "@blueprintjs/core"
 
 type Props = {
   onClick: () => void,
 }
 
 const CleanOrderStorageButtonComponent = ({onClick}: Props) => {
-  return <Button
-    style={{marginTop: 1 + 'px', marginLeft: 7 + 'px'}}
-    className="pt-intent-danger"
-    iconName="pt-icon-eraser"
-    onClick={onClick}
-  />
+  return <Tooltip content="Delete all orders from selected storage in given time range" position={Position.BOTTOM}>
+    <Button
+      style={{marginTop: 1 + 'px', marginLeft: 7 + 'px'}}
+      className="pt-intent-danger"
+      iconName="pt-icon-eraser"
+      onClick={onClick}
+    />
+  </Tooltip>
 }
 
 export default CleanOrderStorageButtonComponent
--- src/Strategy/RunStrategyButtonComponent.js
+++ src/Strategy/RunStrategyButtonComponent.js
@@ -1,18 +1,20 @@
 // @flow
 import React from "react"
-import {Button} from "@blueprintjs/core"
+import {Button, Position, Tooltip} from "@blueprintjs/core"
 
 type Props = {
   onClick: () => void,
 }
 
 const RunStrategyButtonComponent = ({onClick}: Props) => {
-  return <Button
-    style={{marginTop: 1 + 'px', marginLeft: 7 + 'px'}}
-    className="pt-intent-primary"
-    iconName="pt-icon-play"
-    onClick={onClick}
-  />
+  return <Tooltip content="Run simulation of strategy on given time range" position={Position.BOTTOM}>
+    <Button
+      style={{marginTop: 1 + 'px', marginLeft: 7 + 'px'}}
+      className="pt-intent-primary"
+      iconName="pt-icon-play"
+      onClick={onClick}
+    />
+  </Tooltip>
 }
 
 export default RunStrategyButtonComponent
