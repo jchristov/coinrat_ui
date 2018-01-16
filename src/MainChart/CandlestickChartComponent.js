984e380150310af21cfe589aab163e0d5c809005
Petr Hejna <Achse0@gmail.com>
2018-01-16 17:10:40 +0100

    btter text, tooltiop and icon added to legend in dashboard

--- src/MainChart/CandlestickChartComponent.js
+++ src/MainChart/CandlestickChartComponent.js
@@ -1,7 +1,6 @@
 // @flow
 import React, {Component} from 'react'
 import {observer} from "mobx-react"
-import {NonIdealState} from "@blueprintjs/core/dist"
 import CandlesChart from "./CandlesChart"
 import {Flex, Box} from 'reflexbox'
 import {STATUS_CANCELED, STATUS_CLOSED, STATUS_OPEN} from "../Orders/Order"
@@ -10,6 +9,7 @@ import {candleStoreInstance} from "../Candle/CandleStore"
 import {ORDERS_STATUS_COLORS} from "../Orders/ChartColors"
 import {createAggregateFromData} from "./ChartAggregate"
 import {filterStoreInstance} from "../TopLineToolbar/FilterStore"
+import {NonIdealState, Position, Tooltip, Icon, Intent} from "@blueprintjs/core"
 
 const CandlestickChartComponent = observer(class CandlestickChartComponent extends Component<{}> {
 
@@ -36,11 +36,26 @@ const CandlestickChartComponent = observer(class CandlestickChartComponent exten
 
   render() {
     const statuses = ORDERS_STATUS_COLORS
+
+    const mockMarketTooltip = <div style={{maxWidth: 256 + 'px'}}>
+      <p>Mock market is virtual market that simulates behaviour of stock-market.</p>
+      <p>All orders are processed as <strong>closed</strong> immediately.</p>
+    </div>
+
     return (
       <Flex align='center top'>
         <Box auto>{this.renderChart()}</Box>
         <Box w={256}>
-          <p>Strategy executed from UI is <strong>always</strong> executed against <code>mock</code> market.</p>
+          <p>
+            Strategy executed from UI is <strong>always</strong> executed against
+            {' '}
+            <strong>mock market</strong>
+            {' '}
+            <Tooltip content={mockMarketTooltip} position={Position.BOTTOM_RIGHT}>
+              <Icon iconSize={Icon.SIZE_STANDARD} intent={Intent.PRIMARY} iconName="pt-icon-info-sign" />
+            </Tooltip>
+            .
+          </p>
           <p>Executing strategy against real market is possible only via command line on the backend server.</p>
           <h5>Legend</h5>
           <ul className="pt-list-unstyled">
