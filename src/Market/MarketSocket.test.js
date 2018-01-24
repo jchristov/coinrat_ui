c90f27c29cd49ec75b806ce633b85fa5eaa42892
Petr Hejna <Achse0@gmail.com>
2018-01-24 13:14:36 +0100

    add market socket unit test that emit event is called

---
+++ src/Market/MarketSocket.test.js
@@ -0,0 +1,12 @@
+// @flow
+import {MarketSocket} from "./MarketSocket"
+
+it('Load balances calls emit function', () => {
+  const emitFunctionMock = jest.fn()
+  const socketMock = {emit: emitFunctionMock}
+  const candleStorageSocket = new MarketSocket(socketMock)
+  candleStorageSocket.loadMarkets(() => undefined)
+  expect(emitFunctionMock.mock.calls.length).toBe(1)
+  expect(emitFunctionMock.mock.calls[0][0]).toBe('get_markets')
+  expect(emitFunctionMock.mock.calls[0][1]).toEqual({})
+})
