b5c7311a24fc526d613fd8fdd9a2e87c6552d768
Petr Hejna <Achse0@gmail.com>
2018-02-04 22:42:59 +0100

    Revert "Implement MarketPlugin entity, socket and selector"
    This reverts commit 6872295942375a7c279324b2dcfe81f0c97b93b2.

--- src/Pair/PairSocket.js
+++ src/Pair/PairSocket.js
@@ -1,8 +1,8 @@
 // @flow
-import type {AppSocket} from "../Sockets/socket"
+import {AppSocket} from "../Sockets/socket"
 import {SOCKET_EVENT_GET_PAIRS} from "../Sockets/SocketEvents"
 import loadDataForSelectElementStore from "../Sockets/SynchronousDataLoader"
-import type {SelectElement} from "../Form/Select/SelectComponent"
+import {SelectElement} from "../Form/Select/SelectComponent"
 
 type RawPair = {
   key: string,
